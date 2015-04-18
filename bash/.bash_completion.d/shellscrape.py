17d2c42e0d43576aaacb3ab47eefb0c1277585ef
justinnhli <justinnhli@gmail.com>
2015-04-18 17:40:51 -0400

    progress on data-mined bash completion

--- bash/.bash_completion.d/shellscrape.py
+++ bash/.bash_completion.d/shellscrape.py
@@ -3,6 +3,7 @@
 import re
 from os.path import expanduser, realpath
 from collections import Counter, defaultdict
+from math import log
 
 SHELL_HISTORY = realpath(expanduser("~/Dropbox/personal/documents/shell_history"))
 
@@ -24,6 +25,8 @@ def is_weird(line):
     weird |= (line.count('"') % 2 != 0)
     # check for escape sequences (mostly for spaces in paths)
     weird |= (line.count("\\") != 0)
+    # check for environmental variables
+    weird |= ((re.match("[^ ]*=", line)) is not None)
     # check for nested shells
     # TODO eventually these should be extracted and used
     weird |= (line.count("$(") != 0)
@@ -67,7 +70,42 @@ def split_context(command):
             context.append(term)
         else:
             break
-    return " ".join(context), " ".join(terms[len(context):])
+    options = ["-" + term for term in " ".join(terms[len(context):]).split(" -") if term != ""]
+    if len(options) > 0:
+        options[0] = options[0][1:]
+    return " ".join(context), options
+
+def entropy(values):
+    if len(values) == 1:
+        return 1.0
+    total = sum(values)
+    proportions = [value / total for value in values]
+    return sum(-p * log(p, 2) for p in proportions) / log(len(values), 2)
+
+def _prune_by_entropy(path, counts):
+    paths = []
+    if len(counts) > 1:
+        if len(path) > 0:
+            e = entropy([value["__count"] for key, value in counts.items() if key != "__count"])
+            if e < 0.9:
+                paths.append(" ".join(path))
+        for key, value in sorted(counts.items()):
+            if key != "__count":
+                paths.extend(_prune_by_entropy(path + [key,], value))
+    return paths
+
+def prune_by_entropy(contexts):
+    counts = {}
+    counts["__count"] = 0
+    for context in contexts:
+        level_count = counts
+        for term in context.split():
+            if term not in level_count:
+                level_count[term] = {}
+                level_count[term]["__count"] = 0
+            level_count[term]["__count"] += 1
+            level_count = level_count[term]
+    return _prune_by_entropy([], counts)
 
 def main():
     history = []
@@ -80,14 +118,17 @@ def main():
             program = command.split()[0]
             if re.match("[a-zA-Z0-9]", program) and program not in KEYWORDS:
                 history.append(command)
+    contexts = [split_context(command)[0] for command in history if " " in command]
+    print(prune_by_entropy(contexts))
+    exit()
     arguments = defaultdict(Counter)
     for command in history:
         if " " in command:
-            context, args = split_context(command)
+            context, options = split_context(command)
         else:
             context = command
-            args = ""
-        arguments[context].update(args.split())
+            options = ""
+        arguments[context].update(options)
     # TODO need to further break down the context to provide better tab completion
     for context, counter in sorted(arguments.items()):
         distinct = len(counter)
@@ -98,9 +139,11 @@ def main():
         pairs = [pair for pair in counter.most_common() if pair[1] >= average]
         if len(pairs) == 0:
             continue
+        """
         print(context)
         for key, count in sorted(pairs, key=(lambda pair: pair[1]), reverse=True):
             print("\t{}:{}".format(key, count))
+            """
 
 if __name__ == "__main__":
     main()

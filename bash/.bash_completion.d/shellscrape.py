fd3e42c6bfb466266bcc5790b34472a6ab78d691
Justin Li <justinnhli@gmail.com>
2015-07-26 19:53:00 -0400

    redo shellscrape script

--- bash/.bash_completion.d/shellscrape.py
+++ bash/.bash_completion.d/shellscrape.py
@@ -1,60 +1,67 @@
 #!/usr/bin/env python3
 
 import re
-from os.path import expanduser, realpath
+from argparse import ArgumentParser
 from collections import Counter, defaultdict
-from math import log
+from os import getcwd as pwd
+from os.path import expanduser, realpath
+from sys import argv
 
 SHELL_HISTORY = realpath(expanduser("~/Dropbox/personal/documents/shell_history"))
 
-KEYWORDS = ["if", "while", "for"]
+KEYWORDS = ("if", "while", "for", "then", "do")
 
-def read_shell_history():
-    with open(SHELL_HISTORY) as fd:
-        for log in fd.readlines():
-            line = log.strip().split("\t", maxsplit=3)
-            if len(line) == 4:
-                yield(line[3].strip())
+LONG_SHORT_OPTIONS = ("find", "java")
 
-def is_weird(line):
+def is_weird(command):
     weird = False
     # check for unbalanced parentheses
-    weird |= (line.count("(") != line.count(")"))
+    weird |= (command.count("(") != command.count(")"))
     # check for unbalanced quotes
-    weird |= (line.count("'") % 2 != 0)
-    weird |= (line.count('"') % 2 != 0)
+    weird |= (command.count("'") % 2 != 0)
+    weird |= (command.count('"') % 2 != 0)
     # check for escape sequences (mostly for spaces in paths)
-    weird |= (line.count("\\") != 0)
+    weird |= (command.count("\\") != 0)
     # check for environmental variables
-    weird |= ((re.match("[^ ]*=", line)) is not None)
+    weird |= ((re.match("[^ ]*=", command)) is not None)
     # check for nested shells
     # TODO eventually these should be extracted and used
-    weird |= (line.count("$(") != 0)
-    weird |= (line.count("<(") != 0)
+    weird |= (command.count("$(") != 0)
+    weird |= (command.count("<(") != 0)
     return weird
 
-def commands_from_line(line):
-    commands = line.split("|")
-    old_size = len(commands)
-    new_size = -1
-    while old_size != new_size:
-        old_size = new_size
-        new_commands = []
-        for command in commands:
-            new_commands.extend(command.split("&&"))
-        commands = new_commands
-        new_size = len(commands)
-    return [command.strip() for command in commands if not is_weird(command)]
+def read_history():
+    history = set()
+    with open(SHELL_HISTORY) as fd:
+        for entry in fd.readlines():
+            # only take COMMAND from 'DATE HOST PWD COMMAND'
+            entry = entry.strip().split("\t", maxsplit=3)[-1].strip()
+            for line in entry.split(";"):
+                for conjunction in line.split("|"):
+                    for command in conjunction.split("&&"):
+                        if not is_weird(command) and len(command.split()) > 0:
+                            history.add(command.strip())
+    # filter out commands that begin with shell keywords
+    history = set(command for command in history if command.split()[0] not in KEYWORDS)
+    # filter out commands that begin with non-alphabet characters
+    history = set(command for command in history if re.match("[a-z]", command))
+    return history
 
 def simplify_command(command):
     # remove all quoted expressions
-    command = re.sub("'[^']*'", "", command)
-    command = re.sub('"[^\\"]*"', "", command)
+    transformed = command
+    for match in re.finditer("'[^']*'", command):
+        transformed = transformed.replace(match.group(), re.sub("[ \t]", "_", match.group()))
+    command = transformed
+    transformed = command
+    for match in re.finditer('"[^\\"]*"', command):
+        transformed = transformed.replace(match.group(), re.sub("[ \t]", "_", match.group()))
+    command = transformed
     # remove anything with /, ., *, or ~
-    command = re.sub(" [^ ]*(/[^ ]*)/*", "", command)
-    command = re.sub(" [^ ]*(\\.[^ ]*)\\.*", "", command)
-    command = re.sub(" [^ ]*(\\*[^ ]*)\\**", "", command)
-    command = re.sub(" [^ ]*(~[^ ]*)~*", "", command)
+    #command = re.sub(" [^ ]*(/[^ ]*)/*", "", command)
+    #command = re.sub(" [^ ]*(\\.[^ ]*)\\.*", "", command)
+    #command = re.sub(" [^ ]*(\\*[^ ]*)\\**", "", command)
+    #command = re.sub(" [^ ]*(~[^ ]*)~*", "", command)
     # remove all input/output redirections
     command = re.sub(" +>>?.*", "", command)
     command = re.sub(" [^ ]*([<>][^ ]*)[<>]*", "", command)
@@ -62,88 +69,74 @@ def simplify_command(command):
     command = re.sub("( -[^=]+)=([^ ]*)", "\\g<1> \\g<2>", command)
     return command
 
-def split_context(command):
-    terms = command.split()
-    context = []
-    for term in terms:
-        if not term.startswith("-"):
-            context.append(term)
-        else:
-            break
-    options = ["-" + term for term in " ".join(terms[len(context):]).split(" -") if term != ""]
-    if len(options) > 0:
-        options[0] = options[0][1:]
-    return " ".join(context), options
+def operation_analyze():
+    history = read_history()
+    # program -> (option -> values)
+    option_values = defaultdict((lambda: defaultdict(Counter)))
+    for command in history:
+        program = command.split()[0]
+        if program in LONG_SHORT_OPTIONS:
+            continue
+        command = simplify_command(command)
+        previous_option = None
+        for index, argument in enumerate(command.split()):
+            if argument[0] == "-":
+                # new option
+                if len(argument) == 1 or argument == "--" or re.search("[^0-9A-Za-z-]", argument[1:]):
+                    previous_option = None
+                    continue
+                if argument[1] != "-" and re.match("^[0-9A-Za-z-]*$", argument[1:]):
+                    # [conjunction of] short options
+                    for letter in argument[1:-1]:
+                        option_values[program]["-" + letter].update([None,])
+                    argument = "-" + argument[-1]
+                if previous_option is not None:
+                    # previous option had no argument
+                    option_values[program][previous_option].update([None,])
+                if index == len(command.split()) - 1:
+                    option_values[program][argument].update([None,])
+                previous_option = argument
+            elif previous_option is not None:
+                # potential argument for previous option
+                option_values[program][previous_option].update([argument,])
+                previous_option = None
+    option_types = defaultdict(dict)
+    for program, options in sorted(option_values.items()):
+        for option, counter in sorted(options.items()):
+            if None in counter:
+                option_types[program][option] = "none"
+            else:
+                option_types[program][option] = "argument"
+    for program, options in sorted(option_types.items()):
+        print(program)
+        for option, argument_type in sorted(options.items()):
+            print("    {}: {}".format(option, argument_type))
 
-def entropy(values):
-    if len(values) == 1:
-        return 1.0
-    total = sum(values)
-    proportions = [value / total for value in values]
-    return sum(-p * log(p, 2) for p in proportions) / log(len(values), 2)
 
-def _prune_by_entropy(path, counts):
-    paths = []
-    if len(counts) > 1:
-        if len(path) > 0:
-            e = entropy([value["__count"] for key, value in counts.items() if key != "__count"])
-            if e < 0.9:
-                paths.append(" ".join(path))
-        for key, value in sorted(counts.items()):
-            if key != "__count":
-                paths.extend(_prune_by_entropy(path + [key,], value))
-    return paths
+def operation_list():
+    history = read_history()
+    print("\n".join(sorted(set(command.split()[0] for command in history))))
 
-def prune_by_entropy(contexts):
-    counts = {}
-    counts["__count"] = 0
-    for context in contexts:
-        level_count = counts
-        for term in context.split():
-            if term not in level_count:
-                level_count[term] = {}
-                level_count[term]["__count"] = 0
-            level_count[term]["__count"] += 1
-            level_count = level_count[term]
-    return _prune_by_entropy([], counts)
+def operation_complete(context):
+    history = read_history()
+    # find a line that begins with context
+    results = set(command for command in history if command.startswith(context))
+    # remove context from results, leaving the last word
+    context_length = max(context.rfind(" "), context.rfind("\t"))
+    results = set(command[context_length+1:] for command in results)
+    # remove all subsequent words from results
+    results = set(re.sub("[ \t].*", "", command) for command in results)
+    print("\n".join(sorted(results)))
 
 def main():
-    history = []
-    for line in read_shell_history():
-        commands = commands_from_line(line)
-        for command in commands:
-            command = simplify_command(command)
-            if not command:
-                continue
-            program = command.split()[0]
-            if re.match("[a-zA-Z0-9]", program) and program not in KEYWORDS:
-                history.append(command)
-    contexts = [split_context(command)[0] for command in history if " " in command]
-    print(prune_by_entropy(contexts))
-    exit()
-    arguments = defaultdict(Counter)
-    for command in history:
-        if " " in command:
-            context, options = split_context(command)
-        else:
-            context = command
-            options = ""
-        arguments[context].update(options)
-    # TODO need to further break down the context to provide better tab completion
-    for context, counter in sorted(arguments.items()):
-        distinct = len(counter)
-        if distinct == 0:
-            continue
-        total = sum(counter.values())
-        average = total / distinct
-        pairs = [pair for pair in counter.most_common() if pair[1] >= average]
-        if len(pairs) == 0:
-            continue
-        """
-        print(context)
-        for key, count in sorted(pairs, key=(lambda pair: pair[1]), reverse=True):
-            print("\t{}:{}".format(key, count))
-            """
+    arg_parser = ArgumentParser()
+    arg_parser.add_argument("context", nargs="?")
+    args = arg_parser.parse_args()
+    if args.context is None:
+        operation_list()
+    elif args.context[-1] != " ":
+        operation_complete(args.context)
 
 if __name__ == "__main__":
     main()
+    #operation_analyze()

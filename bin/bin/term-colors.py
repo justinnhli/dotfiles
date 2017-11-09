f702016cdabe39e1a32da85e6b41e21d1844cb8c
Justin Li <justinnhli@gmail.com>
2017-11-09 13:17:02 -0800

    [bin] add term-colors.py

---
+++ bin/bin/term-colors.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+# modified from https://askubuntu.com/a/27318
+
+import sys
+
+write = sys.stdout.write
+for i in range(2):
+    for j in range(30, 38):
+        for k in range(40, 48):
+            write("\33[%d;%d;%dm%d;%d;%d\33[m " % (i, j, k, i, j, k))
+        write("\n")

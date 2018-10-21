dddd41079901e1ccb2158c43f443e2803ec50516
Justin Li <justinnhli@gmail.com>
2018-10-20 19:19:40 -0700

    [bin] mark arbitrary dates in ccal

--- bin/bin/ccal.py
+++ bin/bin/ccal.py
@@ -45,32 +45,34 @@ def str_to_date(string, start=True):
         raise ArgumentTypeError(err)
 
 
-def parse_args(start, end):
+def parse_args(args):
     """Parse the start and end date CLI arguments to datetime.date objects.
 
     Arguments:
-        start (str): The start date. Defaults to None.
-        end (str): The end date. Defaults to None.
+        args (Namespace): The command line arguments.
 
     Returns:
-        Tuple[date, date]: The date range described by the arguments.
+        Tuple[date, date, date]: The start date, end date, and marker date.
 
     Raises:
         ArgumentTypeError: If the end date is earlier than the start date.
     """
+    start, end = args.start, args.end
     today = date.today()
+    mark_date = None
     if end is None:
         if start is None:
             start_date = today - 28 * ONE_DAY
             end_date = today + 28 * ONE_DAY
+            mark_date = today
+        elif len(start) == 10:
+            anchor = str_to_date(start)
+            end_date = anchor + 28 * ONE_DAY
+            start_date = anchor - 28 * ONE_DAY
+            mark_date = anchor
         else:
-            if len(start) == 10:
-                anchor = str_to_date(start)
-                end_date = anchor + 28 * ONE_DAY
-                start_date = anchor - 28 * ONE_DAY
-            else:
-                start_date = str_to_date(start)
-                end_date = str_to_date(start, start=False)
+            start_date = str_to_date(start)
+            end_date = str_to_date(start, start=False)
     else:
         start_date = str_to_date(start, start=True)
         end_date = str_to_date(end, start=False)
@@ -79,32 +81,32 @@ def parse_args(start, end):
             f'start date must be before end date,',
             f'but got {start_date} and {end_date}',
         ]))
-    return start_date, end_date
+    if args.no_marker:
+        mark_date = None
+    return start_date, end_date, mark_date
 
 
-def print_calendar(start_date, end_date, marker):
+def print_calendar(start_date, end_date, mark_date=None):
     """Print a compact calendar.
 
     Arguments:
         start_date (date): The start date.
         end_date (date): The end date.
-        marker (bool): Whether to mark today's date. Defaults to True.
+        mark_date (date): The date to mark. Defaults to None.
     """
-    today = date.today()
-    today_date = f'{today.day: >2d}'
-    curr_date = start_date
-    curr_date -= ((curr_date.weekday() + 1) % 7) * ONE_DAY
+    curr_date = start_date - ((start_date.weekday() + 1) % 7) * ONE_DAY
     print(' Su Mo Tu We Th Fr Sa')
     while curr_date <= end_date:
-        output = ' ' + ' '.join(
+        output = ' '.join(
             f'{(curr_date + (i * ONE_DAY)).day: >2d}'
             for i in range(7)
         )
-        if marker and 0 <= (today - curr_date).days <= 7:
-            output = output.replace(f' {today_date} ', f'[{today_date}]')
+        output = f' {output} '
+        if mark_date and 0 <= (mark_date - curr_date).days <= 7:
+            output = output.replace(f' {mark_date.day: >2d} ', f'[{mark_date.day: >2d}]')
         curr_date += 7 * ONE_DAY
         if (curr_date - start_date).days <= 7 or curr_date.day <= 7:
-            output += curr_date.strftime(' %b %Y')
+            output += curr_date.strftime('%b %Y')
         print(output)
 
 
@@ -115,8 +117,7 @@ def main():
     arg_parser.add_argument('end', nargs='?', help='end date')
     arg_parser.add_argument('--no-marker', action='store_true', help='hide today marker')
     args = arg_parser.parse_args()
-    start_date, end_date = parse_args(args.start, args.end)
-    print_calendar(start_date, end_date, not args.no_marker)
+    print_calendar(*parse_args(args))
 
 
 if __name__ == '__main__':

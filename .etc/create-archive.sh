c13af72470ab9574b07ded50a36db5df311bef61
Justin Li <justinnhli@gmail.com>
2018-11-07 23:35:14 -0800

    [etc] update archive script

--- .etc/create-archive.sh
+++ .etc/create-archive.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
-cd "$(dirname "${BASH_SOURCE[0]}")"
-mkdir -p etc/X11/xorg.conf.d/
-cp -f /etc/fstab etc/
-cp -f /etc/pacman.conf etc/
-cp -f /etc/X11/xorg.conf.d/10-marble-mouse.conf etc/X11/xorg.conf.d/
+read -r -d '' paths <<EOF
+/etc/X11/xorg.conf.d/10-marble-mouse.conf
+/etc/fstab 
+/etc/pacman.conf 
+/etc/wpa_supplicant/wpa_supplicant.conf
+/var/spool/cron/justinnhli
+EOF
+
+cd "$(dirname "${BASH_SOURCE[0]}")" || exit
+find . -mindepth 1 -maxdepth 1 -type d -exec rm -rf '{}' ';'
+echo "$paths" | while read -r path; do
+	mkdir -p "$(dirname "$path" | sed 's#^/##')"
+	cp "$path" "$(echo "$path" | sed 's#^/##')"
+done

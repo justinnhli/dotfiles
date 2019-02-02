308bd2a72d64e7a97574ec902f9d8621db68b26b
Justin Li <justinnhli@gmail.com>
2019-02-02 09:28:35 -1000

    [python] support XDG Base Directory spec

---
+++ python/.local/share/python/pythonstartup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+# taken from
+# https://docs.python.org/3/library/readline.html?highlight=readline#example
+
+import atexit
+import os
+import readline
+
+histfile = os.path.join(
+    os.environ.get('XDG_CACHE_HOME', os.path.expanduser('~/.cache')),
+    'python',
+    '.python_history',
+)
+
+try:
+    readline.read_history_file(histfile)
+    h_len = readline.get_current_history_length()
+except FileNotFoundError:
+    open(histfile, 'wb').close()
+    h_len = 0
+
+def save(prev_h_len, histfile):
+    new_h_len = readline.get_current_history_length()
+    readline.set_history_length(1000)
+    readline.append_history_file(new_h_len - prev_h_len, histfile)
+
+atexit.register(save, h_len, histfile)

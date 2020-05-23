bf5d82899862c72d0a51c2f5d484674098e2b4aa
Justin Li <justinnhli@gmail.com>
2020-05-22 18:37:19 -0700

    [pim] finish vimgrep

--- journal.py
+++ journal.py
@@ -511,16 +511,34 @@ def do_readability(journal, args):
 
 @register('list search results in vim :grep format')
 def do_vimgrep(journal, args):
-    entry_locations = {}
+    entry_files = {}
     with open(journal.tags_file) as fd:
         for line in fd.readlines():
-            date, filepath, line_num = line.strip().split('\t')
-            entry_locations[date] = f'{filepath}:{line_num}'
+            date, filepath, _ = line.strip().split('\t')
+            entry_files[date] = filepath
     entries = filter_entries(journal, args)
-    for date, entry in sorted(entries.items()):
-        location = entry_locations[date]
-        snippet = entry.text.replace('\n', ' ')[:40]
-        print(f'{location}:{snippet}')
+    for date, entry in sorted(entries.items(), reverse=True):
+        matches = [re.search(term, entry.text, flags=re.IGNORECASE) for term in args.terms]
+        first_index = min(match.start() for match in matches if match)
+        prev_lines = entry.text[:first_index].splitlines()
+        line_num = len(prev_lines)
+        col_num = len(prev_lines[-1]) + 1
+        filepath = entry_files[date]
+        match_line = entry.text.splitlines()[line_num - 1].strip()
+        if col_num < 20:
+            start_index = 0
+            prefix = ''
+        else:
+            start_index = match_line.rfind(' ', 0, col_num - 20) + 1
+            prefix = '[...] '
+        if col_num > len(match_line) - 40:
+            end_index = len(match_line)
+            suffix = ''
+        else:
+            end_index = match_line.find(' ', col_num + 40)
+            suffix = ' [...]'
+        snippet = match_line[start_index:end_index]
+        print(f'{filepath}:{line_num}:{col_num}:{date} {prefix}{snippet}{suffix}')
 
 
 # CLI

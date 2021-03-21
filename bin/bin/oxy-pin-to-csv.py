6ee28d92ae7d80a478a7b939ec5ccb174caf67e8
Justin Li <justinnhli@gmail.com>
2021-03-20 21:29:23 -0700

    [bin] print student PINs in alphabetical order

--- bin/bin/oxy-pin-to-csv.py
+++ bin/bin/oxy-pin-to-csv.py
@@ -8,11 +8,16 @@ if len(sys.argv) != 2:
     sys.exit(1)
 
 # account for the byte-order-mark
+students = []
 with open(sys.argv[1], encoding='utf-8-sig') as fd:
     for row in DictReader(fd):
         advisors = ', '.join([row['Pri. Adviser'], row['Sec. Advisor']])
         advisors = advisors.strip().strip(',')
-        print(f'{row["Student Name"]} (advisor(s): {advisors})')
-        print(f'    PIN: {row["PIN"]}')
-        print(f'    1st registration time: {row["1st Date"]} {row["1st Time"]} PT')
-        print(f'    2nd registration time: {row["2nd Date"]} {row["2nd Time"]} PT')
+        students.append('\n'.join([
+            f'{row["Student Pref First Name"]} {row["Student Last Name"]} (advisor(s): {advisors})',
+            f'    PIN: {row["PIN"]}',
+            f'    1st registration time: {row["1st Date"]} {row["1st Time"]} PT',
+            f'    2nd registration time: {row["2nd Date"]} {row["2nd Time"]} PT',
+        ]))
+for student in sorted(students):
+    print(student)

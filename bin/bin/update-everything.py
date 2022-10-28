f662ad4d7f82773005f5b2868c7103007d2eba65
Justin Li <justinnhli@gmail.com>
2022-10-28 13:45:05 -0700

    [bin] force pip to upgrade first
    this avoids messages about pip updates, which leads to the json parser failing

--- bin/bin/update-everything.py
+++ bin/bin/update-everything.py
@@ -99,6 +99,7 @@ def update_pip(venv=None):
         pip = Path(environ['PYTHON_VENV_HOME']).joinpath(venv).resolve()
         if not pip.exists():
             return
+    run(['pip', 'install', '--upgrade', 'pip'], check=False)
     process = run(
         [pip, 'list', '--format', 'json'],
         check=True, capture_output=True,

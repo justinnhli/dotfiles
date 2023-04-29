1bfe96c8580c94b9324e56c52586acd96ce4c523
Justin Li <justinnhli@gmail.com>
2023-04-28 22:41:38 -0700

    [bin] allow dynalist.py to download multiple files

--- bin/bin/dynalist.py
+++ bin/bin/dynalist.py
@@ -6,7 +6,8 @@ import re
 from argparse import ArgumentParser
 from collections import namedtuple
 from os import environ
-from typing import Any, Mapping
+from textwrap import indent
+from typing import Any, Optional, Mapping
 
 try:
     import requests
@@ -58,26 +59,30 @@ def list_files():
         },
     )
     assert data['_code'] == 'Ok', f'File listing request received code {data["_code"]}.'
-    return {obj['title']: obj for obj in data['files']}
+    return {
+        obj['title']: obj for obj in data['files']
+        if obj['type'] == 'document'
+    }
 
 
-def get_file_id(filename):
-    # type: (str) -> str
+def get_file_id(filename, files=None):
+    # type: (str, Optional[dict[str, dict[str, str]]]) -> str
     """Get the file ID of a Dynalist file."""
-    files = list_files()
+    if not files:
+        files = list_files()
     if filename not in files:
         raise KeyError(f'Cannot find file with title "{filename}"')
     return files[filename]['id']
 
 
-def get_file_nodes(filename):
-    # type: (str) -> dict[str, dict[str, Any]]
+def get_file_nodes(filename, files=None):
+    # type: (str, Optional[dict[str, dict[str, str]]]) -> dict[str, dict[str, Any]]
     """Get the nodes of a Dynalist file."""
     data = send_json_post(
         'https://dynalist.io/api/v1/doc/read',
         {
             'token': TOKEN,
-            'file_id': get_file_id(filename),
+            'file_id': get_file_id(filename, files),
         },
     )
     assert data['_code'] == 'Ok', f'File contents request failed with code {data["_code"]}.'
@@ -85,21 +90,21 @@ def get_file_nodes(filename):
     return {node['id']: node for node in data['nodes']}
 
 
-def get_file(filename):
-    # type: (str) -> str
+def get_file(filename, files=None):
+    # type: (str, Optional[dict[str, dict[str, str]]]) -> str
     """Get a file from Dynalist."""
-    nodes = get_file_nodes(filename)
+    nodes = get_file_nodes(filename, files)
     assert 'children' in nodes['root']
     treelines = [] # type: list[TreeLine]
     for root_sibling_num, root_id in enumerate(nodes['root']['children'], start=1):
         stack = [(nodes[root_id], 0, root_sibling_num)]
         while stack:
-            node, indent, sibling_index = stack.pop()
+            node, depth, sibling_index = stack.pop()
             node['content'] = re.sub(r'!\(([0-9]{4}-[0-9]{2}-[0-9]{2})\)', r'(\1)', node['content'])
-            treelines.append(TreeLine(node['id'], node['content'], indent, sibling_index))
+            treelines.append(TreeLine(node['id'], node['content'], depth, sibling_index))
             if 'children' in node:
                 stack.extend(
-                    (nodes[child], indent + 1, child_sibling_num)
+                    (nodes[child], depth + 1, child_sibling_num)
                     for child_sibling_num, child
                     in reversed(list(enumerate(node['children'])))
                 )
@@ -113,9 +118,23 @@ def main():
     # type: () -> None
     """Provide an CLI entry point."""
     arg_parser = ArgumentParser()
-    arg_parser.add_argument('remote', help='Dynalist file to read')
+    arg_parser.add_argument('--list', action='store_true', help='list files instead of printing contents')
+    arg_parser.add_argument('--headers', action='store_true', help='always include headers')
+    arg_parser.add_argument('filenames', metavar='filename', nargs='*', help='Dynalist file(s) to read')
     args = arg_parser.parse_args()
-    print(get_file(args.remote))
+    files = list_files()
+    if args.list:
+        print('\n'.join(sorted(files.keys())))
+    else:
+        if not args.filenames:
+            args.filenames = list(files.keys())
+        for filename in args.filenames:
+            contents = get_file(filename, files)
+            if args.headers or len(files) > 1:
+                print(filename)
+                print(indent(contents, '\t'))
+            else:
+                print(contents)
 
 
 if __name__ == '__main__':

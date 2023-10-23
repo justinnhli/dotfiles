f9128e36a474a39bce732d604d58ebe1fab52052
Justin Li <justinnhli@gmail.com>
2023-10-22 17:25:01 -0700

    [bin] improve diff-highlight-finder.sh
    * use `less` directly in the script
    * resolve symlinks more robustly
    * make code more readable
    * add comments

--- bin/bin/diff-highlight-finder.sh
+++ bin/bin/diff-highlight-finder.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 
+# find the path to git
 git_path="$(which git)"
+# if git is a symbolic link, find the real path
+# this should only happen on MacOS
 if [ -L "$git_path" ]; then
-    git_path="$(dirname "$git_path")/$(readlink "$git_path")"
+    # if the path is not an absolute path
+    if ! [[ "$git_path" =~ ^/ ]]; then
+        # create an absolute path by prepending with its parent
+        git_path="$(dirname "$git_path")/$(readlink "$git_path")"
+    fi
+    # normalize the absolute path
+    git_path="$(readlink -f "$git_path")"
 fi
-git_dir_path="$(echo "$git_path" | sed 's#bin#share#; s#git/.*#git#;')"
-diff_highlight_path="$(find "$git_dir_path" -type f -name 'diff-highlight')"
-if [ -z "$diff_highlight_path" ]; then
-    cat
+# go up two directories
+git_path="$(dirname "$(dirname "$git_path")")"
+# find the diff-highlight program
+diff_highlight_path="$(find "$git_path" -type f -name 'diff-highlight')"
+# if the file exists and is executable
+if [ -x "$diff_highlight_path" ]; then
+    "$diff_highlight_path" | less
 else
-    "$diff_highlight_path"
+    less
 fi

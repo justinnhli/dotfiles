b4e20fe9d673e5931fd19a422f12b514694e0973
Justin Li <justinnhli@gmail.com>
2024-09-05 14:04:40 -0700

    [bin] lint timer.py

--- bin/bin/timer.py
+++ bin/bin/timer.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python3
+"""A basic GUI timer."""
 
 from types import SimpleNamespace
 import tkinter as tk
 
+
 class Timer():
+    """A basic GUI timer."""
 
     def __init__(self):
+        # type: () -> None
         self.root = tk.Tk()
         self.root.title('Timer')
         # parameters
@@ -16,23 +20,23 @@ class Timer():
         self.time_input_var = tk.StringVar()
         self.time_input_var.trace(
             'w',
-            (lambda name, index, mode: self.time_input_callback()),
+            (lambda name, index, mode: self._time_input_callback()),
         )
-        self.timing = False
-        self.create_widgets()
+        self.started = False
+        self._create_widgets()
         self.time_input_var.set(self.time_str)
-        self.root.mainloop()
 
-    def create_widgets(self):
+    def _create_widgets(self):
+        # type: () -> None
         textbox = tk.Entry(self.root, textvariable=self.time_input_var, width=10, justify='center')
         textbox.grid(row=0, column=0)
         self.widgets.textbox = textbox
 
-        start_button = tk.Button(self.root, text='Start', command=self.start_timer)
+        start_button = tk.Button(self.root, text='Start', command=self._start_timer)
         start_button.grid(row=0, column=1)
         self.widgets.start_button = start_button
 
-        stop_button = tk.Button(self.root, text='Stop', state='disabled', command=self.stop_timer)
+        stop_button = tk.Button(self.root, text='Stop', state='disabled', command=self._stop_timer)
         stop_button.grid(row=0, column=2)
         self.widgets.stop_button = stop_button
 
@@ -42,9 +46,12 @@ class Timer():
 
     @property
     def time_str(self):
+        # type: () -> str
+        """Format the time remaining."""
         return f'{self.time // 60}:{self.time % 60:02d}'
 
-    def time_input_callback(self):
+    def _time_input_callback(self):
+        # type: () -> None
         time_input = self.widgets.textbox.get()
         parts = time_input.split(':')
         parts = (3 - len(parts)) * ['0'] + parts
@@ -54,16 +61,18 @@ class Timer():
             pass
         self.widgets.label['text'] = self.time_str
 
-    def start_timer(self):
-        self.time_input_callback()
-        self.timing = True
+    def _start_timer(self):
+        # type: () -> None
+        self._time_input_callback()
+        self.started = True
         self.widgets.start_button['state'] = 'disabled'
         self.widgets.stop_button['state'] = 'normal'
         self.widgets.textbox['state'] = 'disabled'
-        self.root.after(self.interval * 1000, self.step)
+        self.root.after(self.interval * 1000, self._step)
 
-    def step(self):
-        if not self.timing:
+    def _step(self):
+        # type: () -> None
+        if not self.started:
             return
         if self.time > 0 and self.widgets.label['fg'] != '#A40000':
             self.time -= self.interval
@@ -71,14 +80,22 @@ class Timer():
             self.time += self.interval
             self.widgets.label['fg'] = '#A40000'
         self.widgets.label['text'] = self.time_str
-        self.root.after(self.interval * 1000, self.step)
+        self.root.after(self.interval * 1000, self._step)
 
-    def stop_timer(self):
-        self.timing = False
+    def _stop_timer(self):
+        # type: () -> None
+        self.started = False
         self.widgets.start_button['state'] = 'normal'
         self.widgets.stop_button['state'] = 'disabled'
         self.widgets.textbox['state'] = 'normal'
         self.widgets.label['fg'] = '#000000'
-        self.time_input_callback()
+        self._time_input_callback()
 
-Timer()
+    def run(self):
+        # type: () -> None
+        """Run the timer."""
+        self.root.mainloop()
+
+
+if __name__ == '__main__':
+    Timer().run()

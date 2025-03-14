57dddd4d42ffac9b90d3fdb1d7c011280a4571f6
Justin Li <justinnhli@gmail.com>
2025-03-14 12:58:57 -0700

    [bin] update PIM location

--- bin/bin/minbib.sh
+++ bin/bin/minbib.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 grep -o 'cite\([Ap]\)\?{[^}]*}' "$@" | sed 's/.*{//; s/}//;' | grep -o '[^,]\+' | sed 's/^ *//; s/ *$//;' | sort -u | while read -r line; do
-	sed -n "/{$line,/,/^$/p" ~/pim/library.bib
+	sed -n "/{$line,/,/^$/p" ~/Dropbox/pim/library.bib
 done
